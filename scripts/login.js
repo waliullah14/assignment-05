2324b8c5a63d2a6bae2627d6603a14fbafe879b3
waliullah14 <waliullah14wu@gmail.com>
2026-03-08 13:01:21 +0600

    completed the login page

---
+++ scripts/login.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById("login-btn");
+btn.addEventListener("click", () => {
+    let username = document.getElementById("username-input").value;
+    let password = document.getElementById("password").value;
+
+    if (username.trim() === 'admin') {
+        username = true;
+    }
+    else {
+        username = false;
+        alert("Wrong Cradentials");
+        return;
+    }
+
+    if (password === "admin123") {
+        password = true;
+    } else {
+        password = false;
+        alert("Wrong Cradentials");
+        return;
+    }
+
+    if (username && password) {
+        window.location.assign("../home.html");
+    }
+})
